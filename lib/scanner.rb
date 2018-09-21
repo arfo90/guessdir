a7fa0477013efa6a90461aaa4f8c0389bef89492
Amireza Fatemi <arfo.amireza@gmail.com>
2018-09-21 20:03:59 +0200

    Handle wrong or none existing targer folder
    in case consumer request for scanning a folder that doesn't exists, it should raise
    NotFoundError expection and let consumer to deal with it

--- lib/scanner.rb
+++ lib/scanner.rb
@@ -1,8 +1,19 @@
 class Guessdir::Scanner
   def initialize(dir = Dir.pwd)
+    begin
+      @dir = Dir.chdir(dir)
+    rescue
+      raise NotFoundError
+    end
   end
 
   def scan
     return { overview: "There is no file in target folder" }
+  end
+end
+
+class NotFoundError < StandardError
+  def initialize(msg="Folder doesn't exists")
+    super
   end
 end
--- spec/scanner_spec.rb
+++ spec/scanner_spec.rb
@@ -4,9 +4,21 @@ describe Guessdir::Scanner do
   describe "#scan" do
     context "Scan method"
 
+    it "raises NotFoundError" do
+      expect { Guessdir::Scanner.new('./itGoesNowWhere') }.to raise_error { |error|
+        expect(error).to be_a(NotFoundError)
+      }
+    end
+    
     it "returns hash with overview that says folder is empty" do
       expect(Guessdir::Scanner.new("#{File.dirname(__FILE__)}/test_resource/empty_folder").scan)
         .to eql({ overview: "There is no file in target folder"})
     end
+=begin
+
+    it "returns hash with overview of files and their size" do
+      expect(Guessdir::Scanner.new("#{File.dirname(__FILE__)}/test_resource/ruby_project").scan)
+        to eql({ overview: "this is ruby project with 100%", ruby: {number_of_files: 3, distro:100}})
+=end
   end
 end
